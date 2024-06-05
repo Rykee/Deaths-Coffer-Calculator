02cece1c279c8f7174db8f2b34a1d41c52e38ca0
Rykee <matrayz@gmail.com>
2024-06-05 13:19:18 +0200

    Fix servers with swagger

--- src/main/java/hu/rhykee/deaths_coffer_calculator/config/BeanConfigs.java
+++ src/main/java/hu/rhykee/deaths_coffer_calculator/config/BeanConfigs.java
@@ -1,19 +1,30 @@
 package hu.rhykee.deaths_coffer_calculator.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+
 import static org.springframework.http.HttpHeaders.USER_AGENT;
 
 @Configuration
-public class RestClientConfigs {
+public class BeanConfigs {
 
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
                 .defaultHeaders(httpHeaders -> httpHeaders.add(USER_AGENT, "DeathsCofferCalculator"))
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://api.deaths-coffer.com");
+        return new OpenAPI().servers(List.of(server));
     }
 
 }
