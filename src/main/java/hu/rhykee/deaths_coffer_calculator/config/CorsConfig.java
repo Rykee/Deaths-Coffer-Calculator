d9824c109204f647f3f893ed3627fea4c6e8a2c1
Rykee <matrayz@gmail.com>
2024-06-14 14:55:26 +0200

    Cors config

--- src/main/java/hu/rhykee/deaths_coffer_calculator/config/CorsConfig.java
+++ src/main/java/hu/rhykee/deaths_coffer_calculator/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package hu.rhykee.deaths_coffer_calculator.config.mongo;
+package hu.rhykee.deaths_coffer_calculator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
