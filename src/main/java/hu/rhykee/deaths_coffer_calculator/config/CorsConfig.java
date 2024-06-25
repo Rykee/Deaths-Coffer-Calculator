2530a90699aca839ed981099f2c080ed5ed54e5c
Rykee <matrayz@gmail.com>
2024-06-25 20:04:46 +0200

    CORS try 2

--- src/main/java/hu/rhykee/deaths_coffer_calculator/config/CorsConfig.java
+++ src/main/java/hu/rhykee/deaths_coffer_calculator/config/CorsConfig.java
@@ -3,16 +3,20 @@ package hu.rhykee.deaths_coffer_calculator.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebSecurity
 public class CorsConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(AbstractHttpConfigurer::disable)  // Disable CORS
+                .cors(withDefaults())  // Disable CORS
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
